5d1a1a97c3ad368c5550428ad0c3138a01b6d365
Saleh <gghost158@gmail.com>
2021-01-22 00:39:28 +0330

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+$(window).on('load', function() {
+    "use strict";
+
+    /*=========================================================================
+        Preloader
+    =========================================================================*/
+    $("#preloader").delay(350).fadeOut('slow');
+    // Because only Chrome supports offset-path, feGaussianBlur for now
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+    if(!isChrome) {
+        document.getElementsByClassName('infinityChrome')[0].style.display = "none";
+        document.getElementsByClassName('infinity')[0].style.display = "block";
+    }
+
+
+  
+
+    /*=========================================================================
+     Parallax layers
+     =========================================================================*/
+     if ($('.parallax').length > 0) { 
+      var scene = $('.parallax').get(0);
+      var parallax = new Parallax(scene, { 
+        relativeInput: true,
+      });
+    }
+
+     /*=========================================================================
+     Text Rotating
+     =========================================================================*/
+    $(".text-rotating").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "bounceIn",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 4000,
+        complete: function () {
+            // Called after the entrance animation is executed.
+        }
+    });
+
+});
